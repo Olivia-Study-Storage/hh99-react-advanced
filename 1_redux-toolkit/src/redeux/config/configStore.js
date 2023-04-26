ec8e00ca4afb01c508d19a32bd18f94d7208d6eb
hansololiviakim <dev.hansolkim@gmail.com>
2023-04-26 18:43:22 +0900

    Note: couter 프로그램으로 Redux - RTK 코드 비교

--- 1_redux-toolkit/src/redeux/config/configStore.js
+++ 1_redux-toolkit/src/redeux/config/configStore.js
@@ -1,9 +1,19 @@
 import { createStore, combineReducers } from 'redux';
 import counter from '../modules/counter';
+import { configureStore } from '@reduxjs/toolkit';
 
-const rootReducer = combineReducers({
-  counter,
+// * AS-IS: Basic Reducer
+// const rootReducer = combineReducers({
+//   counter,
+// });
+// const store = createStore(rootReducer);
+
+// * TO-BE: Redux Toolkit
+const store = configureStore({
+  reducer: {
+    // counter: counter일 경우 한 단어로 생략 가능
+    counter,
+  }
 });
-const store = createStore(rootReducer);
 
 export default store;
--- 1_redux-toolkit/src/redeux/modules/counter.js
+++ 1_redux-toolkit/src/redeux/modules/counter.js
@@ -1,21 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 // * Action Value: action을 변수 형태인 상수로 만듦 (휴먼 에러 방지)
-const PLUS_NUMBER = 'counter/ADD_NUMBER';
-const MINUS_NUMBER = 'counter/MINUS_NUMBER';
+// const PLUS_NUMBER = 'counter/ADD_NUMBER';
+// const MINUS_NUMBER = 'counter/MINUS_NUMBER';
 
 // * Action Creator: action의 value(type)을 return하도록 함수로 선언
-export const plusNumber = (payload) => {
-  return {
-    type: PLUS_NUMBER,
-    payload,
-  }
-};
+// export const plusNumber = (payload) => {
+//   return {
+//     type: PLUS_NUMBER,
+//     payload,
+//   }
+// };
 
-export const minusNumber = (payload) => {
-  return {
-    type: MINUS_NUMBER,
-    payload,
-  }
-}
+// export const minusNumber = (payload) => {
+//   return {
+//     type: MINUS_NUMBER,
+//     payload,
+//   }
+// };
 
 // * 초기 상태값 설정
 const initialState = {
@@ -23,19 +25,33 @@ const initialState = {
 };
 
 // * Reducer: 변화를 일으키는 함수
-const counter = (state = initialState, action) => {
-  switch (action.type) {
-    case PLUS_NUMBER:
-      return {
-        number: state.number + action.payload,
-      };
-    case MINUS_NUMBER:
-      return {
-        number: state.number - action.payload,
-      }
-    default:
-      return state;
-  }
-};
+// const counter = (state = initialState, action) => {
+//   switch (action.type) {
+//     case PLUS_NUMBER:
+//       return {
+//         number: state.number + action.payload,
+//       };
+//     case MINUS_NUMBER:
+//       return {
+//         number: state.number - action.payload,
+//       }
+//     default:
+//       return state;
+//   }
+// };
 
-export default counter;
+// * 아래의 세가지(name, initialState, reducer)를 인자로 가지는 API
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    plusNumber: (state, action) => {
+      state.number = state.number + action.payload;
+    },
+    minusNumber: (state, action) => {
+      state.number = state.number - action.payload;
+    },
+  },
+});
+export default counterSlice.reducer;
+export const { plusNumber, minusNumber } = counterSlice.actions;
