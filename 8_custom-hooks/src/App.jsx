ce1ff55ce357efc58718c2609d2c65f75475c252
hansololiviakim <dev.hansolkim@gmail.com>
2023-04-29 14:41:48 +0900

    Note: 커스텀 훅

--- 8_custom-hooks/src/App.jsx
+++ 8_custom-hooks/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import useInput from './hooks/useInput';
+
+// * 기존 useState로만 모든 것을 관리했을 때의 문제점
+// input의 개수가 증가하면 useState와 이벤트 핸들러도 같이 증가해 코드의 중복이 생긴다!
 
 function App() {
+  const [name, onChnageNameHandler] = useInput();
+  const [password, onChangePasswordHandler] = useInput();
   return (
-    <div>App</div>
+    <div>
+      <input
+        type="text"
+        value={name}
+        onChange={onChnageNameHandler}
+      />
+      <input
+        type="text"
+        value={password}
+        onChange={onChangePasswordHandler}
+      />
+    </div>
   )
 }
 
